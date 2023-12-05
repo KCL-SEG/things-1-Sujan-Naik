8aae9f0ddcbc96821f60855246a227a9c7504e7e
Sujan Naik <sujannaik047@gmail.com>
2023-12-05 14:26:09 +0000

    Refine the thing model

--- things/models.py
+++ things/models.py
@@ -1,9 +1,17 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import Model, CharField
 
 
 # Create your models here.
 class Thing(Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=50)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+
+    def validate_range(self, value):
+        if value < 0 or value > 100:
+            raise ValidationError(
+                message=f'{value} is not between 0 and 100 (inclusive)',
+                params={"value": value},
+            )
+    quantity = models.IntegerField(unique=False, validators=[validate_range])

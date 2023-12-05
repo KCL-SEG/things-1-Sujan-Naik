5c4cab895ef81520303c53cd5dd909b32c828505
Sujan Naik <sujannaik047@gmail.com>
2023-12-05 14:36:29 +0000

    Fix

--- things/models.py
+++ things/models.py
@@ -4,14 +4,16 @@ from django.db.models import Model, CharField
 
 
 # Create your models here.
+def validate_range(value):
+    if value < 0 or value > 100:
+        raise ValidationError(
+            message=f'{value} is not between 0 and 100 (inclusive)',
+            params={"value": value},
+        )
+
+
 class Thing(Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=120, unique=False, blank=True)
 
-    def validate_range(self, value):
-        if value < 0 or value > 100:
-            raise ValidationError(
-                message=f'{value} is not between 0 and 100 (inclusive)',
-                params={"value": value},
-            )
     quantity = models.IntegerField(unique=False, validators=[validate_range])
